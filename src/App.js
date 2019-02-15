0ac1a9b9444e65c5c9e45633aecdf0e0aa61b123
LoriDeCoder <ldavis1005@gmail.com>
2019-02-15 14:39:21 -0600

    Added Header component and AddTodo component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './Components/Todos';
+import Header from './Components/Layout/Header';
+import AddTodo from './Components/AddTodo';
 
 import './App.css';
 
@@ -40,19 +42,31 @@ class App extends Component {
    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: 7,
+      title: "finish this app!",
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <div className="container">        <header className="App-header">
         <img
-            className="App-logo"
+            className="App-Logo"
             src={require("./Images/LDeCoderLOGO.png")}
-            width='auto'
-            height="50%"
-          />         
+            width="auto"
+            height="100%"
+          />
+                  <Header />
+                  <AddTodo addTodo={this.addTodo} />
         </header>
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      </div>
       </div>
     );
   }
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Header () {
+    return (
+        <header>
+            <h1>This is THE to-DONE List</h1>
+        </header>
+    )
+}
+
+export default Header;
