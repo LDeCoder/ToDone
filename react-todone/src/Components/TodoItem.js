aff826e72f3050b72284ff6d573632f0ae77564b
Lori Davis <40526409+LDeCoder@users.noreply.github.com>
2019-02-21 17:52:58 -0600

    Added files via upload
    System currently will not push to Git from VS Code - creating a new branch so that once I figure out the malfunction I can merge the two branches.

---
+++ react-todone/src/Components/TodoItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+
+    getStyle = () => {
+       return {
+        textDecoration: this.props.todo.completed ?
+        'line-through' : 'none',
+        background: 'azure',
+        padding: '10px',
+        borderBottom: '10px #ccc dotted'
+       }
+        }
+
+
+  render() {
+      const { title, id } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+        <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> 
+        {title}
+        <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>~X~</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background:  'darkgrey',
+    color: 'teal',
+    padding: '5px 10px',
+    border: '3px darkgrey solid',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
+
+export default TodoItem
